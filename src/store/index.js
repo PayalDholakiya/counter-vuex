a4c389f3221a90af72a574a5acd44dac170abbd1
payal-12-3 <payaldholakiya312@gmail.com>
2021-03-16 15:23:08 +0530

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    counter: 0,
+  },
+  getters: {
+    counter: (state) => state.counter,
+  },
+
+  mutations: {
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      if (state.counter == 0) {
+        state.counter = 0
+      } else {
+        state.counter--
+      }
+    },
+  },
+  actions: {
+    Increment({ commit }) {
+      commit('increment')
+    },
+    Decrement({ commit }) {
+      commit('decrement')
+    },
+  },
+  modules: {},
+})
